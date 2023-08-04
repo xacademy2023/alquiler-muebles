aeea48b4644a4dcd00bbc923d987f3878409adc0
Your Name <perezfernando95@gmail.com>
2023-08-04 00:05:18 -0300

    Probando deploy backend: La venganza

--- backend/dist/db/connection.js
+++ backend/dist/db/connection.js
@@ -2,18 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 //Conexion local
-// const sequelize = new Sequelize("grupo10", "root", "", {
-//   host: "localhost",
-//   dialect: "mysql",
-// });
-//Conexion para Produccion
-const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new sequelize_1.Sequelize("grupo10", "root", "", {
+    host: "localhost",
     dialect: "mysql",
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: true,
-        },
-    },
 });
+//Conexion para Produccion
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: true,
+//       },
+//     },
+//   }
+// );
 exports.default = sequelize;
--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -2,26 +2,26 @@ import { Sequelize } from "sequelize";
 
 //Conexion local
 
-// const sequelize = new Sequelize("grupo10", "root", "", {
-//   host: "localhost",
-//   dialect: "mysql",
-// });
+const sequelize = new Sequelize("grupo10", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 //Conexion para Produccion
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true,
-      },
-    },
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: true,
+//       },
+//     },
+//   }
+// );
 
 export default sequelize;
