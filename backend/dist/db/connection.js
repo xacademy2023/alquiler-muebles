24bf77bd4c42653497c2cb5779fd10cb96393d84
Your Name <perezfernando95@gmail.com>
2023-08-04 03:10:12 -0300

    Probando deploy de backend

--- backend/dist/db/connection.js
+++ backend/dist/db/connection.js
@@ -1,10 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
+const mysql2_1 = __importDefault(require("mysql2"));
 //Conexion local
 const sequelize = new sequelize_1.Sequelize("grupo10", "root", "", {
     host: "localhost",
     dialect: "mysql",
+    dialectModule: mysql2_1.default,
 });
 //Conexion para Produccion
 // const sequelize = new Sequelize(
--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 
 //Conexion local
 
 const sequelize = new Sequelize("grupo10", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  dialectModule: mysql2,
 });
 
 //Conexion para Produccion
