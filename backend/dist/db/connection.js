6029fe3c116617491dc16ad7370b294deae86b18
Fernando Pérez <88584244+Fergabrielp@users.noreply.github.com>
2023-08-22 22:48:09 -0300

    Borrado de dist
    Borre el dist para evitar y conflictos.

--- backend/dist/db/connection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('grupo10', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-exports.default = sequelize;
