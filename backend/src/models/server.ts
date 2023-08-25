312e645205adb64312915c34b3fc8c63cabc7aaf
Your Name <perezfernando95@gmail.com>
2023-08-25 15:28:58 -0300

    Cambio minimo

--- backend/src/models/server.ts
+++ backend/src/models/server.ts
@@ -4,9 +4,9 @@ import routesUser from "../routes/userRoutes";
 import routesCategory from "../routes/categoryRoutes";
 import cors from "cors";
 import sequelize from "../db/connection";
-import { Product } from './product';
-import { User } from './user';
-import { Category } from './category';
+import { Product } from "./product";
+import { User } from "./user";
+import { Category } from "./category";
 
 class Server {
   private app: Application;
@@ -40,10 +40,9 @@ class Server {
 
   async dbConnect() {
     try {
-      //await sequelize.sync({ alter: true });
-      await Product.sync({ alter:true })
-            await User.sync({ alter:true });
-            await Category.sync({ alter:true });
+      await Category.sync({ alter: true });
+      await Product.sync({ alter: true });
+      await User.sync({ alter: true });
     } catch (error) {
       console.error("Unable to connect to the database:", error);
     }
