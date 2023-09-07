17d950bbe8609ce7463a9a565da05b0aec399dfb
laurafernandezk <laurafernandezk@gmail.com>
2023-09-07 16:20:37 -0300

    se agrega seeder de productos

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("grupo10", "root", "root", {
+const sequelize = new Sequelize("grupo10", "root", "roor", {
   host: "localhost",
   dialect: "mysql",
 });
