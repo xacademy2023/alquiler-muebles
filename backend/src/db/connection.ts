2027e74ed89883aac87223a4c8e1205d3ef3f213
Your Name <perezfernando95@gmail.com>
2023-09-08 03:15:04 -0300

    Agregando nuevo modelo de productos con array de imagenes

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("grupo10", "root", "roor", {
+const sequelize = new Sequelize("grupo10", "root", "root", {
   host: "localhost",
   dialect: "mysql",
 });
