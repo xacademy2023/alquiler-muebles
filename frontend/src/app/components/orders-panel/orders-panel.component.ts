e4563c8fc228fc8254edc3cf410fd909b5a5bc41
Your Name <perezfernando95@gmail.com>
2023-10-13 21:42:52 -0300

    Funcion de aceptar y rechazar ordenes

--- frontend/src/app/components/orders-panel/orders-panel.component.ts
+++ frontend/src/app/components/orders-panel/orders-panel.component.ts
@@ -1,35 +1,24 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { MatDialog } from '@angular/material/dialog';
 import { OrderDialogComponent } from './order-dialog/order-dialog.component';
 import { Order } from '../../interfaces/orders';
 import { OrderService } from '../../services/order.service';
 
-// inProgress, sent, --> ((accepted, rejected))
 @Component({
   selector: 'app-orders-panel',
   templateUrl: './orders-panel.component.html',
-  styleUrls: ['./orders-panel.component.css']
+  styleUrls: ['./orders-panel.component.css'],
 })
 export class OrdersPanelComponent implements OnInit {
-
-
   loading: boolean = false;
   ordersList: Order[] = [];
- /* 
-  ordersList: Order[] = [
-    { id: 1, products: [{ id: 1, name: "silla", prize: 3},], status: 'sent', user: { name: "juan", id: 33, email: "juan@gmail.com", password: 12345, role: "comprador" }, userId: 1, productId: 7 },
-    { id: 1, products: [{ id: 1, name: "silla", prize: 3},], status: 'sent', user: { name: "juan", id: 33, email: "juan@gmail.com", password: 12345, role: "comprador" }, userId: 1, productId: 7 },
-    { id: 1, products: [{ id: 1, name: "silla", prize: 3},], status: 'sent', user: { name: "juan", id: 33, email: "juan@gmail.com", password: 12345, role: "comprador" }, userId: 1, productId: 7 },
-    { id: 1, products: [{ id: 1, name: "silla", prize: 3},], status: 'sent', user: { name: "juan", id: 33, email: "juan@gmail.com", password: 12345, role: "comprador" }, userId: 1, productId: 7 },
-    { id: 1, products: [{ id: 1, name: "silla", prize: 3},], status: 'sent', user: { name: "juan", id: 33, email: "juan@gmail.com", password: 12345, role: "comprador" }, userId: 1, productId: 7 },
-  ];
-  */
 
   constructor(
     private _orderService: OrderService,
     private toastr: ToastrService,
-    private dialog: MatDialog) { }
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.getOrders();
@@ -37,45 +26,55 @@ export class OrdersPanelComponent implements OnInit {
 
   getOrders(): void {
     this.loading = true;
-    this._orderService.getOrders().subscribe((data: Order[]) => {
-      this.ordersList = data;
+    this._orderService.getOrders().subscribe((orders: Order[]) => {
+      const ordersSent = orders.filter((order) => order.status === 'sent');
+      this.ordersList = ordersSent;
       this.loading = false;
     });
   }
 
   rejectOrder(order: Order): void {
     this.loading = true;
-    this._orderService.updateOrder(order.id, { status: "rejected" }).subscribe(() => {
-      this.getOrders();
-      this.toastr.warning(`Orden (${order}) rechazada con exito!`, 'Orden rechazada');
-      this.loading = false;
-    });    
+    this._orderService
+      .updateOrder(order.id, { status: 'rejected' })
+      .subscribe(() => {
+        this.getOrders();
+        this.toastr.warning(
+          `Orden (${order}) rechazada con exito!`,
+          'Orden rechazada'
+        );
+        this.loading = false;
+      });
   }
 
   acceptOrder(order: Order): void {
     this.loading = true;
-    this._orderService.updateOrder(order.id, { status: "accepted" }).subscribe(() => {
-      this.getOrders();
-      this.toastr.warning(`Orden (${order}) aceptada con exito!`, 'Orden aceptada');
-      this.loading = false;
-    });    
+    this._orderService
+      .updateOrder(order.id, { status: 'accepted' })
+      .subscribe(() => {
+        this.getOrders();
+        this.toastr.success(
+          `Orden (${order}) aceptada con exito!`,
+          'Orden aceptada'
+        );
+        this.loading = false;
+      });
   }
 
   openOrderDialog(order: Order): void {
-    let dialogRef = this.dialog.open(OrderDialogComponent, { 
-      width: "60%", height: "50%",
+    let dialogRef = this.dialog.open(OrderDialogComponent, {
+      width: '60%',
+      height: '50%',
       data: {
-        order: {...order} 
-      }
-    }); 
+        order: { ...order },
+      },
+    });
     dialogRef.afterClosed().subscribe((result) => {
-        if(result === "accept") {
-          this.acceptOrder(result);
-        }
-        else if(result == "reject") {
-          this.rejectOrder(result);
-        }
-        console.log(result);
+      if (result === 'accept') {
+        this.acceptOrder(order);
+      } else if (result == 'reject') {
+        this.rejectOrder(order);
+      }
     });
   }
 }
